8801f9d0c94fc8425e4f2d1b78ca8a2e2f43661a
mlh6167 <mlh6167@163.com>
2021-06-07 17:11:10 +0800

    功能3开发完成

--- test2.ts
+++ test2.ts
@@ -3,5 +3,9 @@ console.log(aaa)
 console.log('我正在开发中')
 console.log('我正在测试删除分支的情况')
 var cc = '合并分支'
+<<<<<<< Updated upstream
 var fix_dug = '20'
 var fix_dug = '21'
+=======
+var m = '我在开发新的功能3'
+>>>>>>> Stashed changes
